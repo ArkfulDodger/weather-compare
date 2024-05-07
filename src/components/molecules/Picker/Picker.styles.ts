3bab58bbc561a4ecb77874d4f657605a532ebe90
Noah Reece <noah.w.reece@gmail.com>
2024-05-07 18:36:17 -0400

    alter picker to use mapped items on android

--- src/components/molecules/Picker/Picker.styles.ts
+++ src/components/molecules/Picker/Picker.styles.ts
@@ -1,9 +1,11 @@
-import { StyleSheet, ViewStyle } from "react-native";
+import { StyleSheet, TextStyle, ViewStyle } from "react-native";
 import { AppTheme } from "@/src/theme/themes";
 
 interface Styles {
   button: ViewStyle;
   modalCard: ViewStyle;
+  item: ViewStyle;
+  itemText: TextStyle;
 }
 
 export const createStyles = (theme: AppTheme): Styles => {
@@ -11,11 +13,20 @@ export const createStyles = (theme: AppTheme): Styles => {
     button: {
       flexDirection: "row",
       justifyContent: "space-between",
+      alignItems: "center",
       padding: 5,
     },
     modalCard: {
       margin: 20,
-      shadowOpacity: 0,
+      padding: 20,
+    },
+    item: {
+      padding: 15,
+      marginHorizontal: 20,
+      alignItems: "center",
+    },
+    itemText: {
+      fontSize: 16,
     },
   });
 };
--- src/components/molecules/Picker/Picker.tsx
+++ src/components/molecules/Picker/Picker.tsx
@@ -3,7 +3,7 @@ import {
   Picker as NativePicker,
   PickerProps,
 } from "@react-native-picker/picker";
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { Card, Icon, Modal, Portal, Text } from "react-native-paper";
 import { Platform, View } from "react-native";
@@ -13,12 +13,13 @@ export type PickerData = { [value: string]: string };
 
 export type Props = PickerProps & {
   data: PickerData;
+  leftText?: string;
 };
 
 // allows the user to select an option from a native picker
 // returns the dropdown picker on Android
 // returns a button opening a picker modal on iOS
-const Picker = ({ data, ...pickerProps }: Props) => {
+const Picker = ({ data, leftText, ...pickerProps }: Props) => {
   const styles = useStyles(createStyles);
 
   // whether picker modal is shown on iOS
@@ -26,24 +27,36 @@ const Picker = ({ data, ...pickerProps }: Props) => {
   const showModal = () => setModalShown(true);
   const hideModal = () => setModalShown(false);
 
-  const pickerItems = Object.entries(data).map(([value, label], i) => (
-    <NativePicker.Item key={i} value={value} label={label} />
-  ));
+  const onItemSelect = useCallback(
+    (itemValue: string | number, itemIndex: number) => {
+      !!pickerProps.onValueChange &&
+        pickerProps.onValueChange(itemValue, itemIndex);
+      modalShown && hideModal();
+    },
+    [pickerProps.onValueChange, modalShown]
+  );
+
+  const pickerItems = Object.entries(data).map(([value, label], i) =>
+    Platform.OS === "ios" ? (
+      <NativePicker.Item key={i} value={value} label={label} />
+    ) : (
+      <TouchableOpacity
+        style={styles.item}
+        key={i}
+        onPress={() => onItemSelect(value, i)}
+      >
+        <Text style={styles.itemText}>{label}</Text>
+      </TouchableOpacity>
+    )
+  );
 
   const picker = useMemo(
     () => (
-      <NativePicker
-        {...pickerProps}
-        onValueChange={(itemValue: string | number, itemIndex: number) => {
-          !!pickerProps.onValueChange &&
-            pickerProps.onValueChange(itemValue, itemIndex);
-          modalShown && hideModal();
-        }}
-      >
+      <NativePicker {...pickerProps} onValueChange={onItemSelect}>
         {pickerItems}
       </NativePicker>
     ),
-    [pickerProps, modalShown]
+    [pickerProps, data, modalShown]
   );
 
   const button = useMemo(
@@ -53,15 +66,18 @@ const Picker = ({ data, ...pickerProps }: Props) => {
           <View style={styles.button}>
             <Text>
               {pickerProps.selectedValue
-                ? data[pickerProps.selectedValue]
-                : "Select a Day"}
+                ? (leftText ? `${leftText} ` : "") +
+                  data[pickerProps.selectedValue]
+                : "Select"}
             </Text>
             <Icon source="chevron-down" size={20} />
           </View>
         </TouchableOpacity>
         <Portal>
           <Modal visible={modalShown} onDismiss={hideModal}>
-            <Card style={styles.modalCard}>{picker}</Card>
+            <Card style={styles.modalCard}>
+              {Platform.OS === "ios" ? picker : pickerItems}
+            </Card>
           </Modal>
         </Portal>
       </>
@@ -69,7 +85,7 @@ const Picker = ({ data, ...pickerProps }: Props) => {
     [pickerProps.selectedValue, modalShown]
   );
 
-  return <>{Platform.OS === "ios" ? button : picker}</>;
+  return <>{Platform.OS === "ios" ? button : button}</>;
 };
 
 export default Picker;
