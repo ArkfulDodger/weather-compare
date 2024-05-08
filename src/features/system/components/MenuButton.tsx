a0e6da55b41729268ae08569945bd48db5d02498
Noah Reece <noah.w.reece@gmail.com>
2024-05-08 00:39:29 -0400

    basic menu button stand-in added

--- src/features/system/components/MenuButton.tsx
+++ src/features/system/components/MenuButton.tsx
@@ -1,13 +1,15 @@
 import { ViewStyle } from "react-native";
+import { IconButton } from "react-native-paper";
 
 type Props = {
   style?: ViewStyle;
 };
 
+// the button which brings up the app's main menu
 const MenuButton = ({ style }: Props) => {
   return (
     <>
-      <></>
+      <IconButton icon="menu" />
     </>
   );
 };
